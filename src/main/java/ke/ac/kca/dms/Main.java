eff8abeb56fa6daa4e232c24c241e5a12882eb16
Wisaji <163581516+Wisaji@users.noreply.github.com>
2026-07-06 15:07:21 +0300

    Update Main.java

--- src/main/java/ke/ac/kca/dms/Main.java
+++ src/main/java/ke/ac/kca/dms/Main.java
@@ -1,1 +1,8 @@
+package ke.ac.kca.dms;
+import ke.ac.kca.dms.service.DocumentService;
 
+public class Main {
+    public static void main(String[] args) {
+        new DocumentService(); // Main is empty. We only use it for running.
+    }
+}
